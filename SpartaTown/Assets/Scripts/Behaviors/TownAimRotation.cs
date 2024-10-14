ec6cae45714e65254ae9255c2504c88079771638
keubung <skdud3390@naver.com>
2024-10-14 12:24:03 +0900

    [Add] Character, Move, Camera, Animation, Map

---
+++ SpartaTown/Assets/Scripts/Behaviors/TownAimRotation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class TownAimRotation : MonoBehaviour
+{
+    [SerializeField] private Transform armPivot;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    private void OnAim(Vector2 direction)
+    {
+        RotateArm(direction);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotz = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotz) > 90f;
+        armPivot.rotation = Quaternion.Euler(0, 0, rotz);
+    }
+}
---
+++ SpartaTown/Assets/Scripts/Behaviors/TownMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TownMovement : MonoBehaviour
+{
+    private TownController controller;
+    private Rigidbody2D movementRigidbody;
+    private CharacterStatsHandler characterStatsHandler;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * characterStatsHandler.CurrentStat.speed;
+
+        movementRigidbody.velocity = direction;
+    }
+}
